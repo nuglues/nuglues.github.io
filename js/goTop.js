bc7fd2780b8d42ba6c477fde96ea1c6ae7404b75
nuglues <492738201@qq.com>
2018-10-19 00:11:03 +0800

    add warFrontLine block

--- js/goTop.js
+++ js/goTop.js
@@ -1,7 +1,7 @@
 function goTop() {
-    window.scrollBy(0, -30);
+    window.scrollBy(-30, -30);
     t = setTimeout('goTop()', 20);
-    if (document.documentElement.scrollTop < 1) {
+    if (document.documentElement.scrollTop + document.documentElement.scrollLeft< 1) {
         clearTimeout(t);
     }
 }
