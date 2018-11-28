daa9098b511262855716dd5d99e74b8323f3ed33
nuglues <406455645@qq.com>
2018-11-28 15:49:44 +0800

    rebuilding

--- js/function_UI.js
+++ js/function_UI.js
@@ -2,14 +2,14 @@
 
 document.onreadystatechange = loading;
 function loading() {
-    const loadingBg = document.getElementById("loadingBg");
-    const loadingItems = document.getElementById("loadingItems");
-    const right_part = document.getElementById("right_part");
+    const loading_bg = document.getElementById("loading-bg");
+    const loading_items = document.getElementById("loading-items");
+    //const right_part = document.getElementById("right_part");
     if (document.readyState === "complete") {
-        loadingBg.className = "loadingBg_Transform";
-        loadingItems.className = "loadingItems_Fadeout";
-        right_part.className = "right-part_Transform";
-        setTimeout(function (){document.body.style.overflow = "auto"}, 500);
+        loading_bg.className = "loading-bg-transform";
+        loading_items.className = "loading-items-fadeout";
+        //right_part.className = "right-part_Transform";
+        setTimeout(function (){document.body.style.overflow = "auto"}, 3000);
     }
     else {
         document.body.style.overflow = "hidden";
