ce850a47abcc9138a17f851726fefaa5bcca31a2
nuglues <406455645@qq.com>
2018-12-10 09:42:20 +0800

    data update 2018-12-10

--- js/data_manualSetting.js
+++ js/data_manualSetting.js
@@ -46,7 +46,7 @@ BFAEmissRewrite[0] =
         BFAEmissaryPool[0][1],BFAEmissaryPool[0][4],BFAEmissaryPool[0][3],BFAEmissaryPool[0][2],BFAEmissaryPool[0][0],BFAEmissaryPool[0][1],BFAEmissaryPool[0][5],
         BFAEmissaryPool[0][4],BFAEmissaryPool[0][3],BFAEmissaryPool[0][2],BFAEmissaryPool[0][0],BFAEmissaryPool[0][5],BFAEmissaryPool[0][1],BFAEmissaryPool[0][3],
         BFAEmissaryPool[0][4],BFAEmissaryPool[0][2],BFAEmissaryPool[0][1],BFAEmissaryPool[0][5],BFAEmissaryPool[0][0],BFAEmissaryPool[0][2],BFAEmissaryPool[0][3],
-        BFAEmissaryPool[0][4]];
+        BFAEmissaryPool[0][4],BFAEmissaryPool[0][1]];
 BFAEmissRewrite[1] =
     [BFAEmissaryPool[1][0],BFAEmissaryPool[1][1],BFAEmissaryPool[1][0],BFAEmissaryPool[1][2],BFAEmissaryPool[1][3],BFAEmissaryPool[1][4],BFAEmissaryPool[1][2],
         BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][4],BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][0],BFAEmissaryPool[1][4],
