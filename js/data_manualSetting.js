bc565bc4b0f34a02ffe1251145720c0be3346b42
nuglues <naturallzh@163.com>
2019-01-05 16:08:47 +0800

    data update 2019-1-5

--- js/data_manualSetting.js
+++ js/data_manualSetting.js
@@ -51,7 +51,7 @@ BFAEmissRewrite[0] =
         BFAEmissaryPool[0][4],BFAEmissaryPool[0][1],BFAEmissaryPool[0][0],BFAEmissaryPool[0][5],BFAEmissaryPool[0][2],BFAEmissaryPool[0][3],BFAEmissaryPool[0][4],
         BFAEmissaryPool[0][1],BFAEmissaryPool[0][5],BFAEmissaryPool[0][0],BFAEmissaryPool[0][2],BFAEmissaryPool[0][4],BFAEmissaryPool[0][3],BFAEmissaryPool[0][1],
         BFAEmissaryPool[0][5],BFAEmissaryPool[0][0],BFAEmissaryPool[0][4],BFAEmissaryPool[0][2],BFAEmissaryPool[0][3],BFAEmissaryPool[0][1],BFAEmissaryPool[0][0],
-        BFAEmissaryPool[0][5],BFAEmissaryPool[0][2],BFAEmissaryPool[0][4],BFAEmissaryPool[0][3],BFAEmissaryPool[0][5],BFAEmissaryPool[0][0]];
+        BFAEmissaryPool[0][5],BFAEmissaryPool[0][2],BFAEmissaryPool[0][4],BFAEmissaryPool[0][3],BFAEmissaryPool[0][5],BFAEmissaryPool[0][0],BFAEmissaryPool[0][1]];
 BFAEmissRewrite[1] =
     [BFAEmissaryPool[1][0],BFAEmissaryPool[1][1],BFAEmissaryPool[1][0],BFAEmissaryPool[1][2],BFAEmissaryPool[1][3],BFAEmissaryPool[1][4],BFAEmissaryPool[1][2],
         BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][4],BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][0],BFAEmissaryPool[1][4],
@@ -64,7 +64,7 @@ BFAEmissRewrite[1] =
         BFAEmissaryPool[1][0],BFAEmissaryPool[1][3],BFAEmissaryPool[1][2],BFAEmissaryPool[1][4],BFAEmissaryPool[1][4],BFAEmissaryPool[1][1],BFAEmissaryPool[1][3],
         BFAEmissaryPool[1][5],BFAEmissaryPool[1][6],BFAEmissaryPool[1][7],BFAEmissaryPool[1][2],BFAEmissaryPool[1][3],BFAEmissaryPool[1][1],BFAEmissaryPool[1][3],
         BFAEmissaryPool[1][2],BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][4],BFAEmissaryPool[1][1],BFAEmissaryPool[1][5],BFAEmissaryPool[1][1],
-        BFAEmissaryPool[1][1],BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][1],BFAEmissaryPool[1][6],BFAEmissaryPool[1][4]];
+        BFAEmissaryPool[1][1],BFAEmissaryPool[1][1],BFAEmissaryPool[1][2],BFAEmissaryPool[1][1],BFAEmissaryPool[1][6],BFAEmissaryPool[1][4],BFAEmissaryPool[1][3]];
 
 const LEGEmissaryPool = ["法罗迪斯宫廷","织梦者","高岭诸族","瓦拉加尔","堕夜精灵","肯瑞托","守望者","抗魔联军","圣光军团","阿古斯防卫军"];
 //                              0           1       2           3           4           5       6       7           8           9
@@ -80,7 +80,7 @@ const LEGEmissRewrite =
         LEGEmissaryPool[2],LEGEmissaryPool[9],LEGEmissaryPool[5],LEGEmissaryPool[0],LEGEmissaryPool[8],LEGEmissaryPool[2],LEGEmissaryPool[4],
         LEGEmissaryPool[9],LEGEmissaryPool[5],LEGEmissaryPool[1],LEGEmissaryPool[3],LEGEmissaryPool[2],LEGEmissaryPool[8],LEGEmissaryPool[7],
         LEGEmissaryPool[1],LEGEmissaryPool[9],LEGEmissaryPool[6],LEGEmissaryPool[3],LEGEmissaryPool[2],LEGEmissaryPool[9],LEGEmissaryPool[0],
-        LEGEmissaryPool[5],LEGEmissaryPool[3],LEGEmissaryPool[2],LEGEmissaryPool[8],LEGEmissaryPool[9],LEGEmissaryPool[0]];
+        LEGEmissaryPool[5],LEGEmissaryPool[3],LEGEmissaryPool[2],LEGEmissaryPool[8],LEGEmissaryPool[9],LEGEmissaryPool[0],LEGEmissaryPool[1]];
 
 const unRecordDays = 61;
 
